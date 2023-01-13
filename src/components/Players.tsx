4aa7bddddf547f40b62488c3767cfac5b7d4e357
Fassi Théo <gtheya@gmail.com>
2023-01-13 21:51:03 +0100

    chore: added comments for instructions

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -9,6 +9,13 @@ interface Props {
 }
 
 const Players: React.FC<Props> = ({ children, className, players, color }) => {
+  // TODO: implementer la barre de boost 
+  // Ajoute une div dans la Card,
+  // width 100% (w-full) et genre h-0.5 ou h-1
+  // essaie de la positionner en bas de la carte mais si tu n'y arrives pas, ne t'embete pas
+  // Met la couleur de la div en fonction de la couleur de la carte, tu peux utiliser la fonction getColorClasses, si tu n'y arrives pas c'est pas grave non plus
+  // Ajouter un "style" sur la div pour lui donner une largeur en fonction du boost du joueur
+  // SPOILER: si tu veux te débrouiller seul, ne regarde pas la fin de la ligne, sinon : scroll un peu sur la droite                                                                                                                          style={{ width: `${p.boost}%` }} 
   return (
     <>
       {players.map((p) => (
