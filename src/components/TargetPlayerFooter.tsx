b277e9ec2385484a4fd0a7b45e7f98d770fc3678
Fassi Théo <gtheya@gmail.com>
2023-01-18 01:43:59 +0100

    feature: added transitions

--- src/components/TargetPlayerFooter.tsx
+++ src/components/TargetPlayerFooter.tsx
@@ -1,15 +1,24 @@
 import type { Player } from "../types/updateState";
+import type { TransitionStatus } from "react-transition-group/Transition";
+
 import Boost from "./Boost";
 import PlayerStats from "./PlayerStats";
 
-interface Props {
+type Props = {
   player: Player;
-}
+  state?: TransitionStatus;
+};
 
-const TargetPlayerFooter: React.FC<Props> = ({ player }) => {
+const TargetPlayerFooter: React.FC<Props> = ({ player, state }) => {
+  if (!state) state = "entered";
+
   return (
     <>
-      <section className="mt-auto flex w-full items-center">
+      <section
+        className={`mt-auto flex w-full items-center transition-all duration-300 ${
+          state === "entered" ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <PlayerStats player={player} />
         <Boost player={player} />
       </section>
